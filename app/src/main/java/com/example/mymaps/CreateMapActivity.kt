4039169ce537f2ca9d43c904acd68c7b39645c3b
Sadorian Robertson <sadorianr@DN0a1e24e0.SUNet>
2021-11-02 17:07:05 -0700

    can add maps with custom titles

--- app/src/main/java/com/example/mymaps/CreateMapActivity.kt
+++ app/src/main/java/com/example/mymaps/CreateMapActivity.kt
@@ -1,14 +1,19 @@
 package com.example.mymaps
 
+import android.app.Activity
 import android.app.Dialog
 import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -17,7 +22,10 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.mymaps.databinding.ActivityCreateMapBinding
+import com.example.mymaps.models.Place
+import com.example.mymaps.models.UserMap
 import com.google.android.gms.maps.model.Marker
+import com.google.android.material.snackbar.Snackbar
 
 private const val TAG = "CreateMapActivity"
 class CreateMapActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -38,6 +46,38 @@ class CreateMapActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        mapFragment.view?.let {
+            Snackbar.make(it, "Long press to add a marke!r", Snackbar.LENGTH_INDEFINITE)
+                .setAction("OK", {})
+                .setActionTextColor(ContextCompat.getColor(this, android.R.color.white))
+                .show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_create_map, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // check that save icon was item selected
+        if (item.itemId == R.id.miSave) {
+            Log.i(TAG, "Save button tapped")
+            if (markers.isEmpty()) {
+                Toast.makeText(this, "There must be at least one marker on the map", Toast.LENGTH_LONG).show()
+                return true
+            }
+            val places = markers.map {marker -> Place(marker.title, marker.snippet, marker.position.latitude, marker.position.longitude) }
+            val userMap = intent.getStringExtra(EXTRA_MAP_TITLE)?.let { UserMap(it, places) }
+            val data = Intent()
+            data.putExtra(EXTRA_USER_MAP, userMap)
+            setResult(Activity.RESULT_OK, data)
+            finish()
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
     /**
@@ -59,15 +99,13 @@ class CreateMapActivity : AppCompatActivity(), OnMapReadyCallback {
             markerToDelete.remove()
         }
 
-
         mMap.setOnMapLongClickListener { latLng ->
             Log.i(TAG, "onMapLongClickListener")
             showAlertDialog(latLng)
         }
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        val siliconValley = LatLng(37.4, -122.2)
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(siliconValley, 10F))
     }
 
     private fun showAlertDialog(latLng: LatLng) {
@@ -89,6 +127,5 @@ class CreateMapActivity : AppCompatActivity(), OnMapReadyCallback {
             markers.add(marker)
             dialog.dismiss()
         }
-
     }
 }
--- app/src/main/java/com/example/mymaps/MainActivity.kt
+++ app/src/main/java/com/example/mymaps/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.example.mymaps
 
 import android.app.Activity
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.UserManager
 import android.util.Log
+import android.view.LayoutInflater
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mymaps.models.Place
 import com.example.mymaps.models.UserMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 const val EXTRA_USER_MAP = "EXTRA_USER_MAP"
@@ -17,42 +24,72 @@ const val EXTRA_MAP_TITLE = "EXTRA_MAP_TITLE"
 private const val REQUEST_CODE = 9
 private lateinit var rvMaps: RecyclerView
 private const val TAG = "MainActivity"
+private lateinit var fabCreateMap: FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var userMaps: MutableList<UserMap>
+    private lateinit var mapAdapter: MapsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val UserMaps = generateSampleData()
+        userMaps = generateSampleData().toMutableList()
 
         // Set layout manager on Recycler View
         rvMaps = findViewById(R.id.rvMaps)
         rvMaps.layoutManager = LinearLayoutManager(this)
 
         // Set adapter on Recycler View
-        rvMaps.adapter = MapsAdapter(this, UserMaps, object: MapsAdapter.OnClickListener {
+        mapAdapter = MapsAdapter(this, userMaps, object: MapsAdapter.OnClickListener {
             override fun onItemClick(position: Int) {
                 Log.i(TAG, "onItemClick $position")
                 // When user taps on view, navigate to new activity
                 val intent = Intent(this@MainActivity, DisplayMapActivity::class.java)
-                intent.putExtra(EXTRA_USER_MAP, UserMaps[position])
+                intent.putExtra(EXTRA_USER_MAP, userMaps[position])
                 startActivity(intent)
             }
         })
+        rvMaps.adapter = mapAdapter
 
-        val fabCreateMap = findViewById<FloatingActionButton>(R.id.fabCreateMap)
+        fabCreateMap = findViewById(R.id.fabCreateMap)
         fabCreateMap.setOnClickListener {
             Log.i(TAG, "FAB clicked")
+            showAlertDialog()
+
+        }
+    }
+
+    private fun showAlertDialog() {
+        val mapFormView = LayoutInflater.from(this).inflate(R.layout.dialog_create_map, null)
+        val dialog = AlertDialog.Builder(this)
+            .setTitle("Map Title")
+            .setView(mapFormView)
+            .setNegativeButton("Cancel", null)
+            .setPositiveButton("OK", null).show()
+
+        dialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener {
+            val title = mapFormView.findViewById<EditText>(R.id.etMapTitle).text.toString()
+            if (title.trim().isEmpty()) {
+                Toast.makeText(this, "Map title must not be empty", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+            // Navigate to create map activity
             val intent = Intent(this@MainActivity, CreateMapActivity::class.java)
-            intent.putExtra(EXTRA_MAP_TITLE, "new map name")
+            intent.putExtra(EXTRA_MAP_TITLE, title)
             startActivityForResult(intent, REQUEST_CODE)
+            dialog.dismiss()
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
             // get new map data from the data
+            val userMap = data?.getSerializableExtra(EXTRA_USER_MAP) as UserMap
+            Log.i(TAG, "onActivity result with new map title ${userMap.title}")
+            userMaps.add(userMap)
+            mapAdapter.notifyItemInserted(userMaps.size - 1)
         }
         super.onActivityResult(requestCode, resultCode, data)
     }
